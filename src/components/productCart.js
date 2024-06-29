434f7dd2bcba7055590cca4e851a4319c7b52b3c
Roni Christian Puno <ronichristianpuno@Ronis-MacBook-Air.local>
2024-06-29 21:47:01 +0800

    react practice with redux

---
+++ src/components/productCart.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import iconCart from '../assets/image/iconCart.jpg'
+import { useSelector, useDispatch } from "react-redux";
+import { addToCart } from "../stores/cart";
+
+const ProductCart = (props) => {
+    const carts = useSelector(store => store.cart.items);
+    console.log(carts)
+    const {id, name, price, image, slug} = props.data;
+    const dispatch = useDispatch();
+    const handleAddToCart = () => {
+        dispatch(addToCart({
+            productId: id,
+            quantity: 1
+        }))
+    }
+
+    return (
+        <div className="bg-hite p-5 rounded-xl shadowsm">
+            <Link to={slug}>
+                <img src={image} alt="" className="w-full h-80 object-cover object-top drop-shadow=[0_80px_30px_#0007]" />
+            </Link>
+            <h3 className="text-2xl py-3 text-center font-medium">{name}</h3>
+            <div className="flex justify-between items-center">
+                <p>
+                    $<span className="text-2xl font-medium">{price}</span>
+                </p>
+                <button 
+                    className="bg-gray-300 p-2 rounded-md text-sm hover:bg-gray-400 flex gap-2"
+                    onClick={handleAddToCart}    
+                >
+                    <img src={iconCart} alt="" className="w-5" />
+                    Add To Cart 
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default ProductCart
---
+++ src/products.js
@@ -0,0 +1,66 @@
+import item1 from './/assets/image/item1.jpg';
+import item2 from './/assets/image/item2.jpg';
+import item3 from './/assets/image/item3.jpg';
+import item4 from './/assets/image/item4.jpg';
+import item5 from './/assets/image/item5.jpg';
+import item6 from './/assets/image/item6.jpg';
+import item7 from './/assets/image/item7.jpeg';
+
+export const products = [
+    {
+        id: 1,
+        name: "headset one",
+        price: 101,
+        image: item1,
+        description: "this is a headset one",
+        slug: "headset-one"
+    },
+    {
+        id: 2,
+        name: "headset two",
+        price: 200,
+        image: item2,
+        description: "this is a headset two",
+        slug: "headset-two"
+    },
+    {
+        id: 3,
+        name: "headset three",
+        price: 250,
+        image: item3,
+        description: "this is a headset three",
+        slug: "headset-three"
+    },
+    {
+        id: 4,
+        name: "headset four",
+        price: 500,
+        image: item4,
+        description: "this is a headset four",
+        slug: "headset-four"
+    },
+    {
+        id: 5,
+        name: "headset five",
+        price: 110,
+        image: item5,
+        description: "this is a headset five",
+        slug: "headset-five"
+    },
+    {
+        id: 6,
+        name: "headset six",
+        price: 320,
+        image: item6,
+        description: "this is a headset six",
+        slug: "headset-six"
+    },
+    {
+        id: 7,
+        name: "headset seven",
+        price: 150,
+        image: item7,
+        description: "this is a headset seven",
+        slug: "headset-seven"
+    },
+]
